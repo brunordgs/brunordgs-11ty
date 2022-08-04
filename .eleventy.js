6b331dbddd4e3e68412f898d24c59d8d13b1440b
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-08-04 14:59:38 -0300

    Add custom font

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy({ './src/_css': '/css' });
+	eleventyConfig.addPassthroughCopy({ './src/_fonts': '/fonts' });
+
 	return {
 		dir: {
 			data: '_data',
 			input: 'src',
-			output: '_site',
 			includes: '_includes',
 			layouts: '_layouts',
 		},
