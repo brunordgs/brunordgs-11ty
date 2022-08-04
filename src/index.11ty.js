c6618c5b7bcb3371ee83f9494188565d2e1efefe
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-08-04 14:58:23 -0300

    Add home and projects page

---
+++ src/index.11ty.js
@@ -0,0 +1,11 @@
+exports.data = {
+	layout: 'base.html',
+	title: 'Javascript and more',
+	metaDescription: 'I love learning new things, helping people and what I do.',
+};
+
+exports.render = function () {
+	return /* html */ `
+		<div>Home page</div>
+	`;
+};
---
+++ src/projects/index.11ty.js
@@ -0,0 +1,10 @@
+exports.data = {
+	layout: 'base.html',
+	title: 'Projects',
+};
+
+exports.render = function () {
+	return /* html */ `
+		<div>Projects.</div>
+	`;
+};
