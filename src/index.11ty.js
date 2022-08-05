2fad058ae03cdd7cadf8f9c47af65a2772222158
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-08-04 23:34:43 -0300

    Start adding home information

--- src/index.11ty.js
+++ src/index.11ty.js
@@ -6,6 +6,23 @@ exports.data = {
 
 exports.render = function () {
 	return /* html */ `
-		<div>Home page</div>
+		<section class="home">
+			<div class="homeAbout">
+				<div>
+					<h1 class="homeAbout__title">Bruno Rodrigues</h1>
+					<h2 class="homeAbout__subtitle">Software Engineer</h2>
+					<p class="homeAbout__miniBio">
+						I'm a
+						<a 
+							href="https://github.com/brunordgs" 
+							target="_blank" 
+							rel="nofollow noreferrer">
+						software engineer</a>
+						and a good burger taster.
+						I love learning new things, helping people and what I do.
+					</p>
+				</div>
+			</div>
+		</section>
 	`;
 };
